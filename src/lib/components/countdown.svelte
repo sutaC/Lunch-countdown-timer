9d7469faab6bc7df435592296b19e1414c19c46a
Catus <catus.pl@gmail.com>
2024-01-13 17:26:13 +0100

    Countdown

--- src/lib/components/countdown.svelte
+++ src/lib/components/countdown.svelte
@@ -1,22 +1,51 @@
 <script lang="ts">
 	import Counter from '$lib/components/counter.svelte';
+
+	let days: number = 14;
+	let hours: number = 0;
+	let minutes: number = 0;
+	let seconds: number = 0;
+
+	const counter = setInterval(() => {
+		seconds--;
+		updateCounter();
+	}, 1000);
+
+	function updateCounter() {
+		if (seconds < 0) {
+			seconds = 59;
+			minutes--;
+		}
+		if (minutes < 0) {
+			minutes = 59;
+			hours--;
+		}
+		if (hours < 0) {
+			hours = 23;
+			days--;
+		}
+		if (days < 0) {
+			clearInterval(counter);
+			days = hours = minutes = seconds = 0;
+		}
+	}
 </script>
 
 <div class="countdown">
 	<div class="counter-container">
-		<Counter count={8}></Counter>
+		<Counter count={days}></Counter>
 		<p class="name">Days</p>
 	</div>
 	<div class="counter-container">
-		<Counter count={23}></Counter>
+		<Counter count={hours % 24}></Counter>
 		<p class="name">Hours</p>
 	</div>
 	<div class="counter-container">
-		<Counter count={55}></Counter>
+		<Counter count={minutes % (24 * 60)}></Counter>
 		<p class="name">Minutes</p>
 	</div>
 	<div class="counter-container">
-		<Counter count={41}></Counter>
+		<Counter count={seconds % (24 * 60 * 60)}></Counter>
 		<p class="name">Seconds</p>
 	</div>
 </div>
